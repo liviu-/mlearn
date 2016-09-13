1c04e485ac3ce7d2106b56065be90c74d330495a
liviu- <hi@liviu.me>
2016-09-13 11:25:19 +0100

    Improve LR API and fix a few bugs

--- learn/linear_regression.py
+++ learn/linear_regression.py
@@ -1,39 +1,61 @@
 """A basic implementation of linear regression"""
-
 import numpy as np
 
-x = np.array([0.5, 1.5, 2, 2.3, 5.6])
-y = np.array([1, 2, 3, 4, 5])
+class LinearRegression():
+    """Basic implementation of linear regression"""
+
+    def fit(self, x, y, lr=0.01, epochs=100):
+        """Calculate linear regression for numpy ndarrays
+        
+        Rows should be data points, and columns should be features
+        """
+        # Add bias
+        x = _append_bias(x)
+        # Extract number of features
+        n_features = x.shape[1]
+        w = _initialise_weights(n_features)
+        for i in range(epochs):
+            error = _mse(y, _activation(x, w))
+            w = _train_epoch(x, y, w, lr)
+        print("Error: {}".format(error))
+        print("Weights: {}".format(w))
+        self.w = w
+
+    def predict(self, x):
+        """Predict based on the weights computed previously"""
+        x = _append_bias(x)
+        return _activation(x, self.w)
+
 
 def _mse(a, b):
     """Compute MSE for 2 vectors"""
     return np.mean(np.square(a - b))
 
 
-def _activation(x, w, b=1):
-    """Activation function"""
-    return w * x + b
+def _activation(x, w):
+    """Activation function (dot product in this case)"""
+    return np.dot(x, w)
 
 
-def _calculate_gradient(x, y, w):
-    """Calculate MSE gradient for the weights provided"""
-    return -2 * np.mean(x * (y - (_activation(x, w))))
+def _partial_derivative_mse(x, y, w):
+    """Calculate partial derivatives for MSE"""
+    new_w = []
+    for feature in x:
+        new_w.append(-2 * np.mean(feature * (y - (_activation(x, w)))))
+    return new_w
 
 
 def _train_epoch(x, y, w, lr):
     """Train for one epoch using gradient descent"""
-    gradient_w = 0
+    gradient_w = np.zeros(x.shape[1])
     for i in range(len(x)):
-        gradient_w += _calculate_gradient(x[i], y[i], w)
+        gradient_w += _partial_derivative_mse(x[i], y[i], w)
     return w - lr * gradient_w
 
-def linear_regression(x, y, lr=0.01, epochs=100):
-    """Calculate linear regression for 1D numpy arrays"""
-    w = 1
-    b = 1
-    for i in range(epochs):
-        y_hat = _activation(x, w, b)
-        error = _mse(y, y_hat)
-        print(error)
-        w = _train_epoch(x, y, w, lr)
-    print(w)
+
+def _initialise_weights(n):
+    return np.random.rand(n)
+
+def _append_bias(x):
+    """Append 1 to each data point"""
+    return np.hstack((x, np.ones((x.shape[0], 1))))
